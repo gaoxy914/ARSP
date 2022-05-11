cb1d1414b0069a144705d74a907b9e7c663b5a7a
gaoxy914 <gaoxy@hit.edu.cn>
2022-05-11 09:51:33 +0800

    transform based algorithm

--- kdtree.hpp
+++ kdtree.hpp
@@ -9,15 +9,15 @@ class KDTree {
         int* indices;
         int cnt;
         Node *left, *right;
-        HyperBox R; // mbr of this node
+        HyperBox* R; // mbr of this node
         Node() : axis(-1), cnt(0), indices(nullptr), left(nullptr), right(nullptr) {}
     };
 
     struct Info {
-        vector<InstanceBase> cap_instances;
         int *sigma;
         double beta;
         int xi;
+        vector<int> cap_instances;
     };
 
     Node *root; // root node;
@@ -38,12 +38,13 @@ class KDTree {
         node->axis = axis;
         node->cnt = npoints;
         node->indices = new int[npoints];
-        node->R = HyperBox(dim, points[*indices].coord);
+        node->R = new HyperBox(dim, points[*indices].coord);
         for (int i = 0; i < npoints; ++ i) {
             node->indices[i] = indices[i];
-            node->R = node->R + HyperBox(dim, points[indices[i]].coord);
+            // *node->R = *node->R + HyperBox(dim, points[indices[i]].coord);
+            node->R->Merge(points[indices[i]].coord);
         }
-        // cout << node->R << endl;
+        // cout << *node->R << endl;
         if (npoints > 1) {
             node->left = BuildRecursive(indices, mid, depth + 1);
             node->right = BuildRecursive(indices + mid, npoints - mid, depth + 1);
@@ -60,20 +61,21 @@ class KDTree {
 
     void CalSkyProbRecursive(const Node* node, Info info, map<int, double>& results) const {
         // update info
-        vector<InstanceBase> instances_pv = info.cap_instances;
-        vector<InstanceBase> instances_v, dominates_v;
+        // cout << node->cnt << endl;
+        vector<int> instances_pv = info.cap_instances;
+        vector<int> instances_v, dominates_v;
         for (auto ins : instances_pv) {
-            if (ins.Dominates(dim, node->R.left_bottom)) { // dominates region
+            if (points[ins].Dominates(dim, node->R->left_bottom)) { // dominates region
                 dominates_v.push_back(ins);
-                if (info.sigma[ins.obj_id] + 1 == ins.prob) {
-                    info.beta *= ins.prob;
+                if (info.sigma[points[ins].obj_id] + 1 == points[ins].prob) {
+                    info.beta *= points[ins].prob;
                     info.xi += 1;
-                    info.sigma[ins.obj_id] += 1;
+                    info.sigma[points[ins].obj_id] += 1;
                 } else {
-                    info.beta *= (ins.prob - info.sigma[ins.obj_id] - 1)/(ins.prob - info.sigma[ins.obj_id]);
-                    info.sigma[ins.obj_id] += 1;
+                    info.beta *= (points[ins].prob - info.sigma[points[ins].obj_id] - 1)/(points[ins].prob - info.sigma[points[ins].obj_id]);
+                    info.sigma[points[ins].obj_id] += 1;
                 }
-            } else if (!node->R.IsPoint() && ins.Dominates(dim, node->R.right_top)) // intersects with region
+            } else if (!node->R->IsPoint() && points[ins].Dominates(dim, node->R->right_top)) // intersects with region
                 instances_v.push_back(ins);
         }
         info.cap_instances = instances_v;
@@ -82,11 +84,12 @@ class KDTree {
             int ins_id = points[index].ins_id, obj_id = points[index].obj_id, prob = points[index].prob;
             if (info.xi > 0) results[ins_id] = 0;
             else results[ins_id] = (1/points[index].prob)*info.beta*prob/(prob - info.sigma[obj_id]);
+            // cout << ins_id << '\t' << results[ins_id] << endl;
         } else {
             if (node->left) CalSkyProbRecursive(node->left, info, results);
             if (node->right) CalSkyProbRecursive(node->right, info, results);
         }
-        for (auto ins : dominates_v) info.sigma[ins.obj_id] -= 1;
+        for (auto ins : dominates_v) info.sigma[points[ins].obj_id] -= 1;
     }
 
 public:
@@ -112,10 +115,13 @@ public:
         map<int, double> results;
         Info info;
         info.sigma = new int[m];
-        memset(info.sigma, 0, m*sizeof(info.sigma));
+        memset(info.sigma, 0, m*sizeof(int));
+        // for (int i = 0; i < m; ++ i) info.sigma[i] = 0;
         info.beta = 1;
         info.xi = 0;
-        info.cap_instances = points;
+        // info.cap_instances = points;
+        info.cap_instances.resize(points.size());
+        iota(begin(info.cap_instances), end(info.cap_instances), 0);
         CalSkyProbRecursive(root, info, results);
         delete[] info.sigma;
         info.sigma = nullptr;
--- transalg.hpp
+++ transalg.hpp
@@ -21,6 +21,7 @@ public:
         objects.resize(m);
     }
 
+
     void LoadData(const char* path) {
         ifstream file((string(path) + to_string(m) + string("/cnt.data")).c_str(), ios::in);
         for (int i = 0; i < m; ++ i) { file >> objects[i].cnt; n += objects[i].cnt; }
@@ -36,7 +37,20 @@ public:
                 for (int k = 0; k < Dim; ++ k) file >> objects[i].instances[j].coord[k];
             }
         }
-        file.close();        
+        file.close();
+    }
+
+    void PrintData() {
+        cout << "m = " << m << "\t n = " << n << endl;
+        for (int i = 0; i < m; ++ i) {
+            cout << "obj " << i << "\t cnt = " << objects[i].cnt << endl;
+            for (int j = 0; j < objects[i].cnt; ++ j) {
+                cout << "id = " << objects[i].instances[j].ins_id << "\t (";
+                for (int k = 0; k < Dim - 1; ++ k) cout << objects[i].instances[j].coord[k] << ", ";
+                cout << objects[i].instances[j].coord[Dim - 1] << ")\t";
+                cout << "prob = " << 1/objects[i].instances[j].prob << endl;
+            }
+        }
     }
 
     void TransAlg(const HyperBox& R, map<int, double>& results) {
@@ -56,25 +70,44 @@ public:
                 new_instances.push_back(new_ins);
             }
         }
-        // cout << "finish transformation.\n";
         // build kd-tree
         KDTree kdtree(dim, m, new_instances);
-        // cout << "finish kdtree construction.\n";
+        // cout << "built\n";
         // calculate skyline probability
         results = kdtree.CalSkyPorb();
+        // cout << "done\n";
         for (auto iter : results)
             if (iter.second != 0)
                 cout << "(" << iter.first << ", " << iter.second << ")\n";
     }
 };
 
+/*
+ * ./trans path m
+ * path: file path
+ * m: number of uncertain tuples
+ */
 int main(int argc, char const *argv[]) {
     map<int, double> results;
-    Dataset D(10);
-    double l[2] = {1, 1};
-    double r[2] = {2, 2};
-    HyperBox R(2, l, r);
+    int m = atoi(argv[2]);
+    Dataset D(m);
+    double l[Dim - 1] = {1, 1};
+    double r[Dim - 1] = {2, 2};
+    HyperBox R(Dim - 1, l, r);
     D.LoadData(argv[1]);
+    // D.PrintData();
+#ifdef _LINUX_
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+#endif
     D.TransAlg(R, results);
+#ifdef _LINUX_
+    long long mtime, seconds, useconds;
+    gettimeofday(&end, NULL);
+    seconds = end.tv_sec - start.tv_sec;
+    useconds = end.tv_usec - start.tv_usec;
+    mtime = seconds*1000000 + useconds;
+    printf("Total time is: %lld\n", mtime);
+#endif
     return 0;
 }
